fc4ace14166522661284ce90a73ce01ce5a87502
Eamon Ramstein <lordeamon@gmail.com>
2018-08-27 13:06:11 +0200

    Add project files.

---
+++ BabyStepz/BabyStepz/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BabyStepz.Models;
+
+namespace BabyStepz.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
