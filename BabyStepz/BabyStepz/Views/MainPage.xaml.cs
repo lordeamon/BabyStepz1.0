cd3feeddbc179a739963dc18159e2c4126bb363d
Eamon Ramstein <lordeamon@gmail.com>
2018-09-03 11:32:00 +0200

    Colorscheme-change functionality is getting formed

--- BabyStepz/BabyStepz/Views/MainPage.xaml.cs
+++ BabyStepz/BabyStepz/Views/MainPage.xaml.cs
@@ -11,8 +11,12 @@ namespace BabyStepz.Views
     {
         public MainPage()
         {
+            //Android - Moves toolbar to bottom of the screen
+            //Android - Enables Pagechange by swiping
             On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
             On<Xamarin.Forms.PlatformConfiguration.Android>().EnableSwipePaging();
+
+            //Initializes the Mainpage - Start the application to the user
             InitializeComponent();
         }
     }
